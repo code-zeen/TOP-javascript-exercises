acff9e0b2ff97f60d75ebd1e589988cdc5edf326
aezeen <aejin.km@gmail.com>
2022-07-27 17:32:37 +0900

    complete exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let result = "";
+  for (let i = 1; i <= str.length; i++) {
+    result += str.substr(str.length - i, 1)
+  }
+  return result;
 };
 
 // Do not edit below this line
