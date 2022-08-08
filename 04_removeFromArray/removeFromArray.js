7fcd5f1b113fcb74886b6904fc3340f49f4ea103
aezeen <aejin.km@gmail.com>
2022-08-08 20:13:21 +0900

    complete exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+function removeFromArray(...args) {
+  let array = args[0];
+  let newArray = [];
 
-};
+  array.forEach(item => {
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  return newArray
+
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  const leapYear = true;
 
+  if (year % 400 == 0) {
+    return leapYear;
+  } else if (year % 4 == 0 && year % 100 != 0) {
+    return leapYear;
+  } else {
+    return false;
+  }
 };
+
+// better answer:
+
+// const leapYears = function(year) {
+// return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0 );
+// };
+
 
 // Do not edit below this line
 module.exports = leapYears;
+
